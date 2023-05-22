6218276386f8296e8321cab8bc53ccf30cab95cd
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-05-22 23:50:14 +0200

    Exercise One

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("Hello world!");
+
+var firstName = "Freddy";
+let lastName = "Beytell";
+let age = 21;
+const subject = "Development";
+
+let greeter = "Hello, my name is " + firstName + " " + lastName + ", I am " + age + " years old and I am studying " + subject;
+console.log(greeter); 
